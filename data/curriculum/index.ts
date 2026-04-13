210919d2419cdae1a452fd6d26940facd77e0571
Rumet Bekan <onurpc2130@gmail.com>
2026-04-14 01:28:29 +0300

    DevApp-ZazaLingo UI eşleşmesi yüksek seviyeye çekildi

--- data/curriculum/index.ts
+++ data/curriculum/index.ts
@@ -1,5 +1,19 @@
 import { TestData } from '../../types/question';
 
+import { word_order_example } from './diger/word_order_example';
+import { matching_example } from './diger/matching_example';
+import { sentence_completion_example } from './diger/sentence_completion_example';
+import { test_1773946534141 } from './diger/l02_t1/test_1773946534141';
+import { test_1773946563942 } from './diger/l03_t1/test_1773946563942';
+import { test_1774209082939 } from './diger/Canamey_/test_1774209082939';
+import { test_1774293529313 } from './diger/Kopula/test_1774293529313';
 
 export const TESTS: Record<string, TestData> = {
+    word_order_example: word_order_example,
+    matching_example: matching_example,
+    sentence_completion_example: sentence_completion_example,
+    test_1773946534141: test_1773946534141,
+    test_1773946563942: test_1773946563942,
+    test_1774209082939: test_1774209082939,
+    test_1774293529313: test_1774293529313,
 };
--- data/theme/tokens/typography.ts
+++ data/theme/tokens/typography.ts
@@ -1,6 +1,6 @@
 export const typography = {
     "buttonTextSize": {
-        "val": 40,
+        "val": 35,
         "unit": "px"
     },
     "headerTitleFontSize": 60,
